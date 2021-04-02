aa154db0d98b0ab03756a5a377661c20db740d3f
Victor Doyle <victor-doyle@outlook.com>
2021-04-01 22:59:02 -0700

    added onMouseEnter + leave animations for buttons and styling changes

--- stream-helper/src/components/MovieCard/MovieCard.js
+++ stream-helper/src/components/MovieCard/MovieCard.js
@@ -2,9 +2,12 @@ import { Button, Card, Elevation } from "@blueprintjs/core";
 import '../../styles/MovieCard.css'
 /* img import */
 import moviePoster from '../../media/moviePoster.png'
+import { useState } from "react";
 
 
 function MovieCard() {
+    const [showToggle, setShowToggle] = useState("none")
+
     return(
         <>
     {/*     <Card interactive={true} elevation={Elevation.TWO} className="movieCardMain" >
@@ -18,11 +21,16 @@ function MovieCard() {
         </Card> */}
 
         <div className="movieCardMain">
-            <img src={moviePoster} className="movieImageCard" />
+            <img src={moviePoster} className="movieImageCard" onMouseEnter={() => setShowToggle("grid")}  />
             <h2><a href="#">Movie Title</a></h2>
             <p>Movie Description</p>
             <h5>Main Actors in Movie</h5>
             <h5>Genre</h5>
+            <span className="movieCardButtons"  style={{display: showToggle}} onMouseLeave={() => setShowToggle("none")}> 
+            <Button className="movieCardIcon bp3-intent-success" icon="tick" text="Save" />
+            <Button className="movieCardIcon bp3-intent-warning" icon="eye-on" text="Watched" />
+            <Button className="movieCardIcon bp3-intent-danger" icon="thumbs-down" text="Discard" />
+            </span>
         </div>
         </>
     )
