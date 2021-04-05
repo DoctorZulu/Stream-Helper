e0cd9eb662e998dcc63d7a77ba2a7fab09a9aa83
Victor Doyle <victor-doyle@outlook.com>
2021-04-04 17:42:43 -0700

    fixed grid styling and refactored button stylings on hover

--- stream-helper/src/components/MovieCard/MovieCard.js
+++ stream-helper/src/components/MovieCard/MovieCard.js
@@ -1,4 +1,4 @@
-import { Button, Card, Elevation } from "@blueprintjs/core";
+import { Button, Card, Elevation, Icon } from "@blueprintjs/core";
 import '../../styles/MovieCard.css'
 /* img import */
 import moviePoster from '../../media/moviePoster.png'
@@ -6,32 +6,43 @@ import { useState } from "react";
 
 
 function MovieCard() {
-    const [showToggle, setShowToggle] = useState("none")
-
+    const [isActive, setIsActive] = useState(false)
+    /* TODO: onClick or onSubmit, setShowToggle("none") */
     return(
         <>
-    {/*     <Card interactive={true} elevation={Elevation.TWO} className="movieCardMain" >
-            <h2><a href="#">Movie Title</a></h2>
-            <p>Movie Description</p>
-            <h5>Main Actors in Movie</h5>
-            <h5>Genre</h5>
-            <Button className="movieCardIcon bp3-intent-success" icon="tick" text="Save" />
-            <Button className="movieCardIcon bp3-intent-warning" icon="eye-on" text="Watched" />
-            <Button className="movieCardIcon bp3-intent-danger" icon="thumbs-down" text="Discard" />
-        </Card> */}
-
         <div className="movieCardMain">
-            <img src={moviePoster} className="movieImageCard" onMouseEnter={() => setShowToggle("grid")}  />
+            <img src={moviePoster} className="movieImageCard" onMouseEnter={() => {setIsActive(true); console.log('hovered')}}  />
             <h2><a href="#">Movie Title</a></h2>
             <p>Movie Description</p>
             <h5>Main Actors in Movie</h5>
             <h5>Genre</h5>
-            <span className="movieCardButtons"  style={{display: showToggle}} onMouseLeave={() => setShowToggle("none")}> 
-            <Button className="movieCardIcon bp3-intent-success" icon="tick" text="Save" />
-            <Button className="movieCardIcon bp3-intent-warning" icon="eye-on" text="Watched" />
-            <Button className="movieCardIcon bp3-intent-danger" icon="thumbs-down" text="Discard" />
-            </span>
+
+            { isActive === true ?
+            
+            <div className="movieButtonContainer" >
+                <div className="saveMovieButton" onClick={() => {setIsActive(false); console.log("clicked save")}} > 
+                <Icon icon="heart" color="red" iconSize={20} />
+                </div>
+                <div className="watchedMovieButton" onClick={() => {setIsActive(false); console.log("clicked watched")}}>
+
+                <Icon icon="eye-on" color="orange" iconSize={20} />
+                </div>
+                <div className="discardMovieButton" onClick={() => {setIsActive(false); console.log("clicked discard")}}>
+
+                <Icon icon="thumbs-down" color="yellow" iconSize={20}/>  
+                </div>
+            </div>
+            
+            :
+            
+            <> </>}
+           
         </div>
+
+
+
+
+
         </>
     )
 }
