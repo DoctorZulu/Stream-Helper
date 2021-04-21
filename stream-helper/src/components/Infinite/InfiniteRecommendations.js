c731c391fd1a15d9d5faa939cbcfed674bf4dee5
victordoyle <victor-doyle@outlook.com>
2021-04-21 15:28:50 -0400

    fixed pagination on movies page

--- stream-helper/src/components/Infinite/InfiniteRecommendations.js
+++ stream-helper/src/components/Infinite/InfiniteRecommendations.js
@@ -4,14 +4,50 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import "../../styles/MovieCard.css";
 
 const InfiniteRecommendations = ({ userMovieRecommendations, onLoadMore }) => {
+
+
   const Mapper = () => (
     <div className="movieCardContainer">
-      {userMovieRecommendations.map((movie, i) => (
+      {finalList.map((movie, i) => (
         <MovieCard {...movie} key={i + 1} />
       ))}
     </div>
   );
   //
+
+  /* console.log("IMP!!!!!!,", userMovieRecommendations)
+ */
+  let uniqueList = [...new Set(userMovieRecommendations)]
+
+  let finalList = [...uniqueList]
+
+  console.log(finalList, "TEEEEEEEE=============")
+
+/*   let filteredProviderMovies = userMovieRecommendations.filter(function (movie) {
+    return movie.categoryId === "Netflix";
+}).map(function (movie, i) {
+    <div className="movieCardContainer">
+      {userMovieRecommendations.map((movie, i) => (
+        <MovieCard {...movie} key={i + 1} />
+      ))}
+    </div>
+}) */
+
+
+
+
+
+
+/*   const filteredMapper = () => (
+    <div className="movieCardContainer">
+      {userMovieRecommendations.map((movie, i) => (
+        <MovieCard {...movie} key={i + 1} />
+      ))}
+    </div>
+  ); */
+  //
+
+
 
   return (
     <>
