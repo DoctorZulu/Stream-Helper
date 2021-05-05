7e11d9ea755b5b063b98ed19cb114954cebd1176
victordoyle <victor-doyle@outlook.com>
2021-05-05 18:22:49 -0400

    merge

--- stream-helper/src/components/MovieTrailer/MovieTrailer.js
+++ stream-helper/src/components/MovieTrailer/MovieTrailer.js
@@ -3,18 +3,12 @@ import React from 'react'
 
 const MovieTrailer = (props) => (
     <>
-   {/*  <iframe title="Trailer" width="100%" height="500%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`}frameBorder="0" 
+
+<iframe title="Trailer" width="100%" height="500%" src={`https://www.youtube.com/embed/${props.currentMovieDetails.movie.trailers[0]}`}frameBorder="0" 
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-
-    </iframe> */}
-
-<iframe title="Trailer" width="100%" height="500%" src={`https://www.youtube.com/results?search_query=${props.currentMovieDetails.movie.title}+trailer`}frameBorder="0" 
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-
     </iframe>
 
-  {/*  <div class="outer-embed-ta"><iframe width="100%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`} allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" class="embed-ta"></iframe></div>
- */}
+
 
 
 
