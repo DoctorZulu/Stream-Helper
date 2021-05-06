8310b4acb07e03f909b6c21c3f52084e04d4a886
smadzelonka <smadzelonka@gmail.com>
2021-05-05 20:39:17 -0700

    trailer

--- stream-helper/src/components/MovieTrailer/MovieTrailer.js
+++ stream-helper/src/components/MovieTrailer/MovieTrailer.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 /* passing down movie id as props */
 
-const MovieTrailer = (props) => (
+const MovieTrailer = ({ currentMovieDetails }) => {
+  const [link, setLink] = useState(
+    currentMovieDetails.movie.trailers1.replace(/['"]+/g, ""),
+  );
+  return (
     <>
-   {/*  <iframe title="Trailer" width="100%" height="500%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`}frameBorder="0" 
+      {/*  <iframe title="Trailer" width="100%" height="500%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`}frameBorder="0" 
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
 
     </iframe> */}
+      {console.log(link)}
+      <iframe
+        title="Trailer"
+        width="100%"
+        height="500%"
+        src={`https://www.youtube.com/watch?v=${link}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      {/*  <div class="outer-embed-ta"><iframe width="100%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`} allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" class="embed-ta"></iframe></div>
+       */}
+    </>
+  );
+};
 
-<iframe title="Trailer" width="100%" height="500%" src={`https://www.youtube.com/results?search_query=${props.currentMovieDetails.movie.title}+trailer`}frameBorder="0" 
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-
-    </iframe>
-
-  {/*  <div class="outer-embed-ta"><iframe width="100%" src={`https://www.traileraddict.com/${props.currentMovieDetails.movie.title}/trailer`} allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" class="embed-ta"></iframe></div>
- */}
-
-
-
-</>
-)
-
-export default MovieTrailer
+export default MovieTrailer;
