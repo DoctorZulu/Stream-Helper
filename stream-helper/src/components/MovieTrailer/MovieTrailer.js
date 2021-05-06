64d061c13bf812984c5cb7f97b25c38d0e5322b2
smadzelonka <smadzelonka@gmail.com>
2021-05-06 11:37:02 -0700

    added you tube videos to movie details

--- stream-helper/src/components/MovieTrailer/MovieTrailer.js
+++ stream-helper/src/components/MovieTrailer/MovieTrailer.js
@@ -16,7 +16,7 @@ const MovieTrailer = ({ currentMovieDetails }) => {
         title="Trailer"
         width="100%"
         height="500%"
-        src={`https://www.youtube.com/watch?v=${link}`}
+        src={`https://www.youtube.com/embed/${link}`}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
