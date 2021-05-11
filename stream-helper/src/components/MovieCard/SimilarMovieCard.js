7e662cf4f0e71e95d361ba5256dd2345ab63d322
victordoyle <victor-doyle@outlook.com>
2021-05-11 15:10:22 -0400

    added similar movies + styling

---
+++ stream-helper/src/components/MovieCard/SimilarMovieCard.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import "../../styles/SimilarMovieCard.css";
+
+function SimilarMovieCard(props) {
+    const [movieTitle, setMovieTitle] = useState();
+    const [movieImage, setMovieImage] = useState();
+
+    console.log("PRRROPOPS", props)
+    return(
+        <>
+        <div className="similarMovieCardLink">
+        <Link to={`/movie/${props.similarMovie.id}`}>
+          <img
+            src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${props.similarMovie.poster_path}`}
+            className="similarMovieCard"
+            alt="movie poster image"
+          />
+        </Link>
+       
+        </div>
+        </>
+    )
+}
+
+export default SimilarMovieCard
