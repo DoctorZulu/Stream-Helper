ed69280adc3585e62300811e00ada6036e6a1122
victordoyle <victor-doyle@outlook.com>
2021-05-14 18:59:06 -0400

    the best version of a 404 possible

--- stream-helper/src/pages/NotFound.js
+++ stream-helper/src/pages/NotFound.js
@@ -1,24 +1,29 @@
 import {  Button, Container, Row } from 'react-bootstrap';
 import React from 'react'
 import { Link } from 'react-router-dom';
+import NotFoundImage from '../media/404page.gif'
+import '../styles/404.css';
 
 function NotFound() {
     return(
         <>
         <Container>
             <Row>
-                <h1> There Appears To Be A Glitch In The Matrix... <br/>
+                <h1 className="wrongPageHeading"> There Appears To Be A Glitch In The Matrix... <br/>
                 You Must Have Wondered Onto An Unexisting Page!</h1>
+            </Row>
+            <Row>
+                <img src={NotFoundImage} alt="404NotFound" className="notFoundImage" />
             </Row>
 
             <Row>
                 <Link to={'/home'}>
-                <Button> Take The Blue Pill</Button>
+                <Button className="notFoundButtons"> Take The Blue Pill</Button>
                 </Link>
 
 
                 <Link to={'/home'}>
-                <Button variant="danger" > Take The Red Pill</Button>
+                <Button className="notFoundButtons" variant="danger" > Take The Red Pill</Button>
                 </Link>
             </Row>
         </Container>
